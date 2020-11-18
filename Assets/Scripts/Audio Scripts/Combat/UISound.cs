6da3d033b1666073175568ee82ad87012902adf6
caurso99 <48004221+caurso99@users.noreply.github.com>
2020-11-18 01:13:41 -0500

    Resolving Commit issues?

---
+++ Assets/Scripts/Audio Scripts/Combat/UISound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UISound : MonoBehaviour, IPointerEnterHandler
+{
+    //Script to control all UI Sounds
+
+    //Button Select Sounds (OnCLick)
+    public void SelectSound()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Combat Events/Select Option");
+    }
+
+    //On Button Hover
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Combat Events/Hover Option");
+    }
+}
