48826fdf3b3beb56b8db2d90793a0fbb6b9be47b
caurso99 <48004221+caurso99@users.noreply.github.com>
2020-11-27 02:21:40 -0500

    Added Audio Menu
    Press P to try it out! Player's can now choose to mute the audio or turn it up. I also made it so they can turn down just the music or just the SFX if they want.

---
+++ Assets/Scripts/Audio Scripts/Audio Menu/AudioSettings.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSettings : MonoBehaviour
+{
+    FMOD.Studio.EventInstance SFXVolumeTest;
+
+    FMOD.Studio.Bus Master;
+    FMOD.Studio.Bus Music;
+    FMOD.Studio.Bus SFX;
+
+    float MasterVolume = 1f;
+    float MusicVolume = 1f;
+    float SFXVolume = 1f;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+
+        Master = FMODUnity.RuntimeManager.GetBus("bus:/Master");
+        Music = FMODUnity.RuntimeManager.GetBus("bus:/Master/Music");
+        SFX = FMODUnity.RuntimeManager.GetBus("bus:/Master/SFX");
+
+        SFXVolumeTest = FMODUnity.RuntimeManager.CreateInstance("event:/Test/Test Sound Effect");
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Master.setVolume(MasterVolume);
+        Music.setVolume(MusicVolume);
+        SFX.setVolume(SFXVolume);
+    }
+
+    public void MasterVolumeLevel(float newMasterVolume)
+    {
+        MasterVolume = newMasterVolume;
+    }
+
+    public void MusicVolumeLevel(float newMusicVolume)
+    {
+        MusicVolume = newMusicVolume;
+    }
+
+    public void SFXVolumeLevel(float newSFXVolume)
+    {
+        SFXVolume = newSFXVolume;
+
+        //makes sure it isn't playing over itself
+        FMOD.Studio.PLAYBACK_STATE PbState;
+        SFXVolumeTest.getPlaybackState(out PbState);
+        if (PbState != FMOD.Studio.PLAYBACK_STATE.PLAYING)
+        {
+            SFXVolumeTest.start();
+        }
+    }
+}
---
+++ Assets/Scripts/Audio Scripts/Audio Menu/PauseMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject audioMenu;
+    public bool menuOpen = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioMenu.SetActive(false);
+        menuOpen = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            OpenMenu();
+        }
+
+        if(menuOpen == true)
+        {
+            audioMenu.SetActive(true);
+        }
+        else
+        {
+            audioMenu.SetActive(false);
+        }
+    }
+
+    public void OpenMenu()
+    {
+        if (menuOpen)
+        {
+            menuOpen = false;
+        }
+        else if (!menuOpen)
+        {
+            menuOpen = true;
+        }
+    }
+}
