ef6e7b065e399bf4675ad2d35ee51205aabb458b
caurso99 <48004221+caurso99@users.noreply.github.com>
2020-11-29 02:40:49 -0500

    Filming Stuff

---
+++ Assets/cameraMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float speed;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(0, speed);
+        }
+
+        if (!Input.GetKey(KeyCode.UpArrow) && !Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            player.SetActive(true);
+        }
+    }
+}
